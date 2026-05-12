cb960e8e8e5cc8bd4979fd9d9093a3273392fba4
Hannes <abelha@chalmers.se>
2026-05-12 10:58:33 +0200

    yehaaaa

---
+++ GoodFridge/src/api/products.js
@@ -0,0 +1,19 @@
+const API_KEY = import.meta.env.VITE_SERPAPI_KEY;
+
+export async function searchProducts(query) {
+  const params = new URLSearchParams({
+    engine: 'google_images',
+    q: query,
+    google_domain: 'google.com',
+    hl: 'en',
+    num: '1',
+    api_key: API_KEY,
+  });
+
+  const res = await fetch(`/serpapi/search?${params}`);
+  const data = await res.json();
+  console.log('SerpApi svar:', data);
+
+  const firstImage = data.images_results?.[0]?.thumbnail;
+  return firstImage ? [{ image: firstImage }] : [];
+}
